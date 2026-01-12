313bf8a0bca4001b56660c6c8a9d82a7bfd93640
lucky <laxman.kompelli@apty.io>
2026-01-12 12:08:40 +0530

    Initial working build form v1

---
+++ build-challenge-form/request.js
@@ -0,0 +1,6 @@
+fetch("https://hooks.zapier.com/hooks/catch/20662197/uwzzk2o/", {
+  body: '{"firstName":"Laxman","lastName":"Kompelli","email":"laxman@apty.ai","phone":"+917878787878","linkedin":"https://linked.com","city":"Hyderabad","teamName":"Apty Team","teamSize":"5","teamMembers":"Laxman - Manager\\n","theme":"ai-agents","themeName":"AI Agents for Enterprise","problem":"DAP Problem","solution":"DAP Platform Solution","whyYou":"DAP Experts","primaryTargetCustomer":"Enterprise customers","whyCustomersChoose":"We are experts","businessModelCanvas":"https://business.com","anythingElse":"Nothing","status":"Employed, planning to leave","superpower":"Technical / Engineering","previousWork":"Apty Dap","github":"https://github.com","demo":"https://demo.com","referralSource":"LinkedIn","termsAccepted":true,"commitmentAccepted":true,"eligibilityAccepted":true,"submittedAt":"2026-01-06T13:41:01.469Z"}',
+  method: "POST",
+  mode: "cors",
+  credentials: "omit",
+});
